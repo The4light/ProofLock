51c3de1b46df04ef7901697ec5609bc9b48b34e4
The4light <ddogbonna08@gmail.com>
2026-02-06 16:04:33 +0100

    switched to ngrok for my backend online instead of render

--- mobile/services/api.ts
+++ mobile/services/api.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = axios.create({
-  // Keep the /api/v1 here...
-  baseURL: 'https://prooflock.onrender.com/api/v1',
+  // Use the dev domain they gave you
+  baseURL: 'https://rosamaria-uncopiable-willfully.ngrok-free.dev/api/v1',
 });
 
 api.interceptors.request.use(async (config) => {
